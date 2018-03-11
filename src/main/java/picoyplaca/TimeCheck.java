180b1cd12b21406a39c912834f5b8e98a0939ba7
JoaquinHervasFarru <joaquinhervasfarru@gmail.com>
2018-03-11 16:41:34 -0500

    Edited afternoon pico y placa start and end times

--- src/main/java/picoyplaca/TimeCheck.java
+++ src/main/java/picoyplaca/TimeCheck.java
@@ -2,7 +2,7 @@ package picoyplaca;
 
 public class TimeCheck {
 	private int time;
-	private int mrningPicoStrt = 700 , mrningPicoEnd = 930, aftnPicoStrt = 1630, aftnPicoEnd = 2000;
+	private int mrningPicoStrt = 700 , mrningPicoEnd = 930, aftnPicoStrt = 1600, aftnPicoEnd = 1930;
 
 	public TimeCheck(int time) {
 		this.time = time;
